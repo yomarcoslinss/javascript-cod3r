6886ec995f2716321574884c7143b889f06bf4a0
yomarcoslinss <vinicius.gzm0@gmail.com>
2022-10-06 06:08:24 -0300

    created exercicios-js

---
+++ Fundamentos/exercicios-js/Fundamentos/comentarios.js
@@ -0,0 +1,13 @@
+//Comentário em uma linha
+console.log("Linha 1");
+
+/*
+Comentário de
+múltiplas linhas
+*/
+console.log("Linha 2");
+/*
+* Comentário de
+* múltiplas línhas
+*/
+console.log("Linha 3");
